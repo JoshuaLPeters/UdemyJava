9eb96cbf0de98db355e594ac6c6cdf1596aac740
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-01 22:01:47 -0600

    saving Udemy Primative type Min Max exercise

---
+++ src/MinAndMax.java
@@ -0,0 +1,40 @@
+public class MinAndMax {
+
+
+
+    public static void main(String[] args) {
+
+        byte myMinByte = Byte.MIN_VALUE;
+        byte myMaxByte = Byte.MAX_VALUE;
+        System.out.println(myMinByte);
+        System.out.println(myMaxByte);
+
+        int myMinInt = Integer.MIN_VALUE;
+        int myMaxInt = Integer.MAX_VALUE;
+        System.out.println(myMaxInt);
+        System.out.println(myMinInt);
+
+        short myMinShort = Short.MIN_VALUE;
+        short myMaxShort = Short.MAX_VALUE;
+        System.out.println(myMinShort);
+        System.out.println(myMaxShort);
+
+        Float myMinFloat = Float.MIN_VALUE;
+        Float myMaxFloat = Float.MAX_VALUE;
+        System.out.println(myMaxFloat);
+        System.out.println(myMinFloat);
+
+        double myMinDouble = Double.MIN_VALUE;
+        double myMaxDouble = Double.MAX_VALUE;
+        System.out.println(myMinDouble);
+        System.out.println(myMaxDouble);
+
+        long myMinLong = Long.MIN_VALUE;
+        long myMaxLong = Long.MAX_VALUE;
+        System.out.println(myMinLong);
+        System.out.println(myMaxLong);
+
+
+
+    }
+}
