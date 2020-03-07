d850dc669778a0208208036790c074a7e2b2b810
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-06 23:54:12 -0600

    inheritance practice with cat object

---
+++ src/InheritancePractice/Cat.java
@@ -0,0 +1,20 @@
+package InheritancePractice;
+
+public class Cat extends Animal {
+
+    private int meowLevel;
+    private int whiskers;
+    private String favoriteBox;
+
+    public Cat(String name, int size, int weight, int meowLevel, int whiskers, String favoriteBox) {
+        super(name, 1, 1, size, weight);
+        this.meowLevel = meowLevel;
+        this.whiskers = whiskers;
+        this.favoriteBox = favoriteBox;
+    }
+
+    @Override
+    public void move(int speed) {
+        super.move(speed);
+    }
+}
