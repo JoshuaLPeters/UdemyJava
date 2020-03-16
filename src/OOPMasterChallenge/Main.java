5689c14ca1178bb6da2454643d47d88d7bd3671d
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-16 18:11:53 -0500

    master OOP exercise challenge save

---
+++ src/OOPMasterChallenge/Main.java
@@ -0,0 +1,18 @@
+package OOPMasterChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", .75);
+        hamburger.addHamburgerAddition3("Cheese",1.13);
+        System.out.println("Total burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.itemizeHamburger();
+        healthyBurger.addHealthAddition1("Lentils",3.41);
+    }
+
+}
