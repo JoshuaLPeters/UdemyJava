ee75d594c9bb97bd73fa27bb1f9bfaaf3d423d19
Joshua Peters <peters.l.joshua@gmail.com>
2020-03-17 20:03:26 -0500

    saving interface review progress

--- src/ArrayExercises/ReferenceVSValue.java
+++ src/ArrayExercises/ReferenceVSValue.java
@@ -1,4 +1,29 @@
-package ArrayExercises;
-
-public class ReferenceVSValue {
-}
+//package ArrayExercises;
+//
+//
+//import java.util.Arrays;
+//
+//public class ReferenceVSValue {
+//
+//    public static void main(String[] args) {
+//        int myIntValue = 10;
+//        int anotherIntValue = myIntValue;
+//        System.out.println("myIntValue = " + myIntValue);
+//        System.out.println("anotherIntValue = " + anotherIntValue);
+//
+//        anotherIntValue++;
+//        System.out.println("myIntValue = " + myIntValue);
+//        System.out.println("anotherIntValue = " + anotherIntValue);
+//
+//        int[] myIntArray = new int[5];
+//        int[] anotherArray = myIntArray;
+//
+//        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+//        System.out.println("anotherIntValue = " + Arrays.toString(anotherArray));
+//
+//        anotherArray[0] = 1;
+//        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+//        System.out.println("anotherIntValue = " + Arrays.toString(anotherArray));
+//
+//    }
+//}
---
+++ src/InterfaceExercises/ITelephone.java
@@ -0,0 +1,13 @@
+package InterfaceExercises;
+
+public interface ITelephone {
+
+    // a class that is going to implement this interface needs these methods
+
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+
+}
